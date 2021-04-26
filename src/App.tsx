6a7da8e3a620819b40203564844a11c4ec4f3755
Dimosthenis Kalaitzis <ftv.voidblaze@gmail.com>
2021-04-26 11:16:50 +0300

    Update SEO data and add favicon

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,11 @@ function App() {
                 and efficient web development. Team player with leading
                 abilities."
         />
+        <meta name="theme-color" content="#b45309" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"
+        ></link>
       </Helmet>
       <Layout>
         <Card>
